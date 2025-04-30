2777dcf8dbfae5ec1d794a72d64b0b1534013ef5
unknown <hendrawanar67@gmail.com>
2025-04-30 11:38:03 +0700

    tes workflow

--- python-ci-test/test_app.py
+++ python-ci-test/test_app.py
@@ -2,8 +2,8 @@ import pytest
 from calculator import add, multiply
 
 def test_add():
-    assert add(3, 2) == 5
-    assert add(-1, 1) == 0
+    assert add(5, 3) == 5
+    assert add(9, 2) == 0
 
 def test_multiply():
     assert multiply(3, 4) == 12
